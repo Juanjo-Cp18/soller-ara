a1e3a22ed7c61b39efd2cc3f124cf72efdb18a64
soller-ara <infosollerara@gmail.com>
2026-09-17 09:53:28 +0200

    fix: carregar la nova gestió de fonts v0.45

--- admin/config.js
+++ admin/config.js
@@ -2,7 +2,7 @@ window.SOLLER_ARA_ADMIN_API = "https://soller-ara-admin.infosollerara.workers.de
 
 window.addEventListener("DOMContentLoaded", () => {
   const script = document.createElement("script");
-  script.src = "./sources.js?v=0.44";
+  script.src = "./sources.js?v=0.45";
   script.defer = true;
   document.body.appendChild(script);
 });
